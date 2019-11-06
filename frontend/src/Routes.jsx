4ceb19698a048e08e8aa370d587784b3a21cb6c6
Prince <prince.ibewiro@cellereIT.com.br>
2019-11-06 07:34:34 -0300

    addicionar algums funcionalidade

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -57,7 +57,7 @@ export default class Routes extends Component {
           <>
             <Router>
               <Switch>
-                  <PrivateRouteUser exact path="/add_stores" component={AddStore} />
+                  <PrivateRouteUser exact path="/stores" component={Stores} />
                   <PrivateRouteUser exact path="/novo_produtos" component={NewProducts} />
                   <PrivateRouteUser exact path="/new_store" component={NewStore} />
                   <PrivateRouteUser exact path="/mercados" component={Mercados} />
@@ -76,7 +76,7 @@ export default class Routes extends Component {
                     <PrivateRouteUser exact path="/choose_profile" component={ChooseProfile} />
                     <PrivateRouteUser exact path="/profilePJ" component={ProfilePJ} />
                     <PrivateRouteUser exact path="/profile_admin" component={ProfileAdmin} />
-                    <PrivateRouteUser exact path="/add_stores" component={AddStores} />
+                    {/* <PrivateRouteUser exact path="/add_stores" component={AddStores} /> */}
                     <PrivateRouteUser exact path="/profile_entregador" component={ProfileEntregador} />
                     <Route exact path="/admin/20192019" component={SigninAdmin}/>
                     <PrivateRouteUser exact path="/mercados/:store_url" component={MercadoList} />

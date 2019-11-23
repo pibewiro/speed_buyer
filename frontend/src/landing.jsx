b38c275b037e72bb85b5e63e6f6af0b9ea8491af
prince <pibewiro@yahoo.com>
2019-11-23 18:38:13 -0300

    deploy

--- frontend/src/landing.jsx
+++ frontend/src/landing.jsx
@@ -22,7 +22,7 @@ export default class landing extends Component {
             <div id="showcase">
                <Navbar />
                <div className="showcase-content">
-                    <h1>Buy Foods From The Comfort of Your House</h1>
+                    <h1>Buyyy Foods From The Comfort of Your House</h1>
                     <p>A new way to deliver food</p>
                     <div>
                         <Link to="/signin" className="btn" >Sign-In</Link>

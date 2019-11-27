d9bad3e674c83ed4dbfc87150fd540c9779ca525
Rafael nunes <studiosafeway@gmail.comcd>
2019-11-27 19:06:40 -0300

    Mudei cadastro

--- speedbuyerApp/src/navigators/MainNavigator.js
+++ speedbuyerApp/src/navigators/MainNavigator.js
@@ -11,7 +11,7 @@ const MainNavigator = createBottomTabNavigator({
     Login:{
                 screen:HomeStack
             },
-            Profile:{
+            Cadastro:{
                 screen:Profile
             }
     });
